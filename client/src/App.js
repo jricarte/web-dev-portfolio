ce8b6a5d5fe6eef2ea830173d2b52945fe44ee27
Jordan Ricarte <jordan@ricarte.net>
2019-11-20 17:26:37 -0600

    boilerplate for portofolio w/o backend

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Discover from "./pages/Discover";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <Wrapper>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/discover" component={Discover} />
+            <Route exact path="/search" component={Search} />
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Discover.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class Discover extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Make New Friends</h1>
+        <h3 className="text-center">
+          Thumbs up on any pups you'd like to meet!
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Discover;
